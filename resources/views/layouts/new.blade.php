16503331325aa2ab1c961523a2ac93dac7530414
Dennis Mutuma <dennocapi@gmail.com>
2020-10-31 12:39:02 +0300

    First commit

---
+++ resources/views/layouts/new.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel ="stylesheet" href="{{asset('css/app.css')}}">
+        <title>{{config('app.name'),'LSAPP'}}</title>
+    </head>
+    <body>
+        @include('inc.navbar')
+        <div class='container'>
+            @include('inc.messages')
+            @yield('content')
+        </div>
+    <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.ckeditor').ckeditor();
+        });
+    </script>
+    </body>
+</html>
